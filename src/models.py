6e3f2a0162a2dc67d9a41ab21a2e8a17a5221a94
Blancreyes <luijbr83@gmail.com>
2023-02-11 10:16:33 +0000

    Third Commit

--- src/models.py
+++ src/models.py
@@ -28,7 +28,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
@@ -41,16 +41,12 @@ class User(Base):
 
 class Follower(Base):
     __tablename__ = 'follower'
-# Here we define columns for the table person
-# Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))    
 
 class Media(Base):
     __tablename__ = 'media'
-# Here we define columns for the table person
-# Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     type = Column(Enum)
     url = Column(String(250), nullable=False)
@@ -59,8 +55,6 @@ class Media(Base):
 
 class Post(Base):
     __tablename__ = 'post'
-# Here we define columns for the table person
-# Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     comment = relationship("Comment", backref="post", lazy=True)
@@ -68,9 +62,6 @@ class Post(Base):
 
 class Comment(Base):
     __tablename__ = 'comment'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
